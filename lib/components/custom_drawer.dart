a1c15f78169f0dd9054ff20dac72af27df2ea561
Trương Khắc Dĩ <tkhacdi.26@gmai.com>
2024-06-28 23:03:19 +0700

    Create Menu drawer

---
+++ lib/components/custom_drawer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              const DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    color: Colors.blue,
+                    size: 40,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: const Text("H O M E"),
+                  leading: const Icon(Icons.home),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text("S E T T I N G S"),
+                  leading: Icon(Icons.settings),
+                ),
+              ),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              left: 25.0,
+              bottom: 25.0,
+            ),
+            child: ListTile(
+              title: Text("L O G O U T"),
+              leading: Icon(Icons.logout),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_chat/auth/auth_service.dart';
+import 'package:flutter_app_chat/components/custom_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,11 +14,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home page"),
+        title: const Center(
+          child: Text("Home page"),
+        ),
         actions: [
           IconButton(onPressed: logout, icon: const Icon(Icons.logout))
         ],
       ),
+      drawer: const CustomDrawer(),
     );
   }
 }
